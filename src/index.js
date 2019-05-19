836efbbd9d2b6195ca17702277cdb4c271d77aee
Sandeep Tikoo <sandeep.tikoo@gmail.com>
2019-05-19 20:33:05 +0800

    array custom types and wildcard search

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 import{ GraphQLServer} from 'graphql-yoga'
-import { getMaxListeners } from 'cluster';
+import { getMaxListeners } from 'cluster'
+// import { v1 as uui } from 'uuid'
+import { v4 as uuid } from 'uuid'
 
-//Types in GraphQL
-//1.String, 2. Boolian, 3. Int 4. Float, 5. ID (to represent unique Identifier) 
-//convention Uppercase first letter for custom types in Graphql
+//Demo user/posts data
+const users = [{
+        id: uuid(),
+        name: 'Sandeep',
+        email: 'sandeep@example.com',
+
+    },
+    {
+        id: uuid(),
+        name: 'Arnav',
+        email: 'arnav@example.com',
+        age: 5
+    },
+    {
+        id: uuid(),
+        name: 'Kiran',
+        email: 'kiran@example.com',
+        age: 33
+    }
+]
+const posts = [{
+        id: uuid(),
+        title: 'Hello!',
+        body: 'How are you doing',
+        published: true
+
+    },
+    {
+        id: uuid(),
+        title: 'Heya!',
+        body: 'How was your day at school?',
+        published: false
+    },
+    {
+        id: uuid(),
+        title: 'Hi There!',
+        body: 'Are we meeting tonight?',
+        published: true
+    }
+]
+
+// -------------------------------------------------
+
+// Type Defs
 const typeDefs = `
     type Query  {
-        me: User!
+        users(query: String): [User!]!
+        posts(query: String): [Post!]!
+        me(id: ID): User!
         post: Post!
     }
 
@@ -29,22 +74,51 @@ const typeDefs = `
 
 const resolvers =   {
     Query:  {
-        me()    {
-            return {
-                id: '123098',
-                name: 'Sandeep Tikoo',
-                email: 'san.tik@example.com',
-                age: 28
+        me(parent,args,ctx,info)    {
+            if (args.id === '1')   {
+                return {
+                    id: 1,
+                    name: 'Sandeep Tikoo',
+                    email: 'san.tik@example.com',
+                    age: 28
+                }
+            } else  {
+                return {
+                    id: 'default id',
+                    name: 'Sandeep Tikoo',
+                    email: 'san.tik@example.com',
+                    age: 28
+                }
             }
         },
-        post()    {
+        post(parent,args,ctx,info)    {
             return {
-                id: '444098',
+                id: uuid(),
                 title: 'Heya!',
                 body: 'How are you doing today?',
                 published: true
             }
+        },
+        users(parent,args,ctx,info) {
+            if (!args.query) {
+                return users
+            }
+            return users.filter((user)   =>  {
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
+            })
+            // return users
+        },
+        posts(parent,args,ctx,info) {
+            if (!args.query) {
+                return posts
+            }
+            return posts.filter((post)   =>  {
+                return post.title.toLowerCase().includes(args.query.toLowerCase())
+            })
+            // return users
         }
+
+
     }
 }
 
---
+++ src/old-beg/indexOld4.js
@@ -0,0 +1,91 @@
+import{ GraphQLServer} from 'graphql-yoga'
+import { getMaxListeners } from 'cluster'
+// import { v1 as uui } from 'uuid'
+import { v4 as uuid } from 'uuid'
+
+//Types in GraphQL
+//1.String, 2. Boolian, 3. Int 4. Float, 5. ID (to represent unique Identifier) 
+//convention Uppercase first letter for custom types in Graphql
+const typeDefs = `
+    type Query  {
+        greeting(name: String, position: String): String!
+        me: User!
+        post: Post!
+        add(a: Float!, b: Float): Float!
+        grades: [Int!]!
+    }
+
+    type User   {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+    }
+
+    type Post   {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+    }
+`
+
+//Resolvers
+
+const resolvers =   {
+    Query:  {
+        me()    {
+            return {
+                id: uuid(),
+                name: 'Sandeep Tikoo',
+                email: 'san.tik@example.com',
+                age: 28
+            }
+        },
+        post()    {
+            return {
+                id: uuid(),
+                title: 'Heya!',
+                body: 'How are you doing today?',
+                published: true
+            }
+        },
+        // Resolver functions receive 4 types of arguments when invoked by client
+        //1. parent - ?
+        //2. args - Arguments
+        //3. ctx - Context
+        //4. info - >
+        greeting(parent, args, ctx, info)    {
+            if (args.name && args.position)    {
+                console.log('both!')
+                return `Hello,  ${args.name}, position.. ${args.position}! `
+            } else if (args.name)  {
+                console.log('name!')
+                return `Hello,  ${args.name}!`
+            } else if (args.position) {
+                console.log('position!')
+                return 'Hello ' + args.position
+            } else  {
+                console.log('none!')
+                return 'Hello!'
+            }
+        },
+        add(parent, args, ctx, info)  {
+            return args.a + args.b
+        },
+        grades(parent, args, ctx, info)    {
+            return [99,80,93]
+        }
+    }
+}
+
+
+const server = new GraphQLServer({
+    typeDefs: typeDefs,
+    resolvers: resolvers
+})
+
+server.start(() =>  {
+    console.log('The server up and running!')
+    
+})
